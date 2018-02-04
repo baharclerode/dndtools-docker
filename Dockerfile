ec79ed6bb51f4e9db6b238827d944f6c2d66f58c
Bryan Harclerode <baharclerode@gmail.com>
2018-02-03 22:31:59 -0600

    Initial Checkin

---
+++ Dockerfile
@@ -0,0 +1,25 @@
+FROM python:2.7-alpine3.7 AS builder
+
+# Get custom version of django-reversion
+COPY django-reversion-1.3.3.zip /django-reversion-1.3.3.zip
+RUN pip install /django-reversion-1.3.3.zip
+
+RUN apk add --no-cache build-base git
+RUN git clone https://github.com/dndtools/dndtools.git /dndtools
+
+WORKDIR /dndtools/
+ARG revision=master
+RUN git checkout --detach $revision
+RUN pip install -r requirements.txt
+
+COPY local.py dndtools/dndproject/local.py
+
+FROM python:2.7-alpine3.7
+
+WORKDIR /dndtools/dndtools/
+ENTRYPOINT ["/usr/local/bin/python2.7", "manage.py"]
+CMD ["runserver"]
+EXPOSE 8000
+
+COPY --from=builder /dndtools/ /dndtools/
+COPY --from=builder /usr/local/lib/python2.7/site-packages/ /usr/local/lib/python2.7/site-packages/
---
+++ local.py
@@ -0,0 +1,48 @@
+# -*- coding: utf-8 -*-
+
+import os
+
+# Change this to False if you run it in development
+DEBUG = True
+TEMPLATE_DEBUG = DEBUG
+
+# See https://docs.djangoproject.com/en/1.6/ref/settings/#databases for reference
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': os.environ['MYSQL_DATABASE'],
+        'USER': os.environ['MYSQL_USER'],
+        'PASSWORD': os.environ['MYSQL_PASSWORD'],
+        'HOST': os.getenv('MYSQL_HOST','127.0.0.1'),
+        'PORT': '',
+        'OPTIONS': {
+            'init_command': 'SET storage_engine=INNODB',
+            },
+        }
+}
+
+# Make this unique, and don't share it with anybody.
+SECRET_KEY = os.getenv('SECRET_KEY', 'TODO FILL')
+
+SITE_ID = 1
+
+if DEBUG:
+    INTERNAL_IPS = ('127.0.0.1',)
+
+    DEBUG_TOOLBAR_PANELS = (
+        'debug_toolbar.panels.versions.VersionsPanel',
+        'debug_toolbar.panels.timer.TimerPanel',
+        'debug_toolbar.panels.settings.SettingsPanel',
+        'debug_toolbar.panels.headers.HeadersPanel',
+        'debug_toolbar.panels.request.RequestPanel',
+        'debug_toolbar.panels.sql.SQLPanel',
+        'debug_toolbar.panels.staticfiles.StaticFilesPanel',
+        'debug_toolbar.panels.templates.TemplatesPanel',
+        'debug_toolbar.panels.cache.CachePanel',
+        'debug_toolbar.panels.signals.SignalsPanel',
+        'debug_toolbar.panels.logging.LoggingPanel',
+        'debug_toolbar.panels.redirects.RedirectsPanel',
+    )
+
+RECAPTCHA_PUBLIC = 'TODO FILL'
+RECAPTCHA_PRIVATE = 'TODO FILL'
